a8c1842bb8a07702745bbf689c86d1df31536c88
Karel <Karel4ever@gmail.com>
2020-09-20 19:59:01 +0200

    Custom App Bar

--- lib/screens/nav_screen.dart
+++ lib/screens/nav_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_facebook_responsive_ui/screens/home_screen.dart';
+import 'package:flutter_facebook_responsive_ui/data/data.dart';
+import 'package:flutter_facebook_responsive_ui/screens/screens.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
@@ -29,9 +30,22 @@ class _NavScreenState extends State<NavScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
     return DefaultTabController(
       length: _icons.length,
       child: Scaffold(
+        appBar: Responsive.isDesktop(context)
+            ? PreferredSize(
+                preferredSize: Size(screenSize.width, 100.0),
+                child: CustomAppBar(
+                  currentUser: currentUser,
+                  icons: _icons,
+                  selectedIndex: _selectedIndex,
+                  onTap: (index) => setState(() => _selectedIndex = index),
+                ),
+              )
+            : null,
+
         // Maintains scroll position
         body: IndexedStack(
           index: _selectedIndex,
@@ -41,15 +55,18 @@ class _NavScreenState extends State<NavScreen> {
         //  physics: NeverScrollableScrollPhysics(),
         //  children: _screens,
         //),
-        bottomNavigationBar: Container(
-          padding: const EdgeInsets.only(bottom: 8.0), // iOS
-          color: Colors.white,
-          child: CustomTabBar(
-            icons: _icons,
-            selectedIndex: _selectedIndex,
-            onTap: (index) => setState(() => _selectedIndex = index),
-          ),
-        ),
+
+        bottomNavigationBar: !Responsive.isDesktop(context)
+            ? Container(
+                padding: const EdgeInsets.only(bottom: 8.0), // iOS
+                color: Colors.white,
+                child: CustomTabBar(
+                  icons: _icons,
+                  selectedIndex: _selectedIndex,
+                  onTap: (index) => setState(() => _selectedIndex = index),
+                ),
+              )
+            : const SizedBox.shrink(),
       ),
     );
   }
